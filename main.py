8468819dc5483e4a0477cd34db1f4b1dcc20b805
Samarthya <87315517+samarthya04@users.noreply.github.com>
2023-05-20 15:58:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+# Import required libraries
+import cv2
+import numpy as np
+import dlib
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Connects to your computer's default camera
+cap = cv2.VideoCapture(0)
+
+# Detect the coordinates
+hog = cv2.HOGDescriptor()
+detector = hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# detector = dlib.get_frontal_face_detector()
+
+def detect(frame):
+    bounding_box_cordinates, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+
+    person = 1
+    for x, y, w, h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        person += 1
+        if person > 10:
+            cv2.putText(frame, 'Please go to other door ', (40, 100), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 2)
+
+    cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, 'Q : Quit', (500, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Capture frames continuously
+while True:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame = detect(frame)
+
+    # This command let's us quit with the "q" button on a keyboard.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and destroy the windows
+cap.release()
+cv2.destroyAllWindows()
