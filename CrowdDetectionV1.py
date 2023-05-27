99bbb724f9b507a3edd1f1ff457c19af392770c5
Samarthya <87315517+samarthya04@users.noreply.github.com>
2023-05-27 20:27:27 +0530

    Add files via upload

---
+++ CrowdDetectionV1.py
@@ -0,0 +1,52 @@
+# Import required libraries
+import pickle
+import cv2
+import numpy as np
+
+# Connects to your computer's default camera
+cap = cv2.VideoCapture(0)
+
+# Detect the coordinates
+hog = cv2.HOGDescriptor()
+detector = hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+# detector = dlib.get_frontal_face_detector()
+
+def detect(frame):
+    bounding_box_cordinates, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+
+    person = 1
+    for x, y, w, h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        person += 1
+        if person > 10:
+            cv2.putText(frame, 'Please go to other door ', (40, 100), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 2)
+
+    cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, 'Q : Quit', (500, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
+
+
+# Capture frames continuously
+i = 0
+frame_lst = []
+while i<11:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame_lst.append(frame)
+    frame = detect(frame)
+    i+=1
+    # This command let's us quit with the "q" button on a keyboard.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+with open("frames1", "wb") as fp:
+    pickle.load("frames1")
+# Release the capture and destroy the windows
+cap.release()
+cv2.destroyAllWindows()
